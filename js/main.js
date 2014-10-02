ce9b9cdc4e4fb7f221a905da0fd3cdfb18cfb022
Marta <trac0040@algonquinlive.com>
2014-10-02 12:05:42 -0400

    Duplicated query for right side

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var sales1 = $('.sales1');
 var salesD1 = $('.sales__details1');
+var sales2 = $('.sales2');
+var salesD2 = $('.sales__details2');
 
 var buttonClickHandler = function () {
 	var currentState = salesD1.attr('data-state');
@@ -12,3 +14,15 @@ var buttonClickHandler = function () {
 };
 
 sales1.on('click', buttonClickHandler);
+
+var buttonClickHandler = function () {
+	var currentState = salesD2.attr('data-state');
+	
+	if (currentState == 'active') {
+		salesD2.attr('data-state', 'inactive')
+	} else {
+		salesD2.attr('data-state', 'active')	
+	}
+};
+
+sales2.on('click', buttonClickHandler);
